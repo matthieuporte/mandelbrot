5c66c591d761828085f1fe623f2bfc2f032dff10
Matthieu Porte <matthieu@famille-porte.eu>
2023-12-17 18:08:55 +0100

    gtk interface

---
+++ Makefile
@@ -0,0 +1,23 @@
+# Makefile
+
+# Flags for implicit rules
+CC = gcc -fsanitize=address
+CPPFLAGS = -MMD
+CFLAGS = -g -lm -fsanitize=address -Wall -Wconversion -Wextra `pkg-config --cflags gtk+-3.0`
+LDLIBS = `pkg-config --libs gtk+-3.0`
+
+
+OBJ = main.o
+DEP = ${OBJ:.o=.d}
+
+all: main
+
+main: ${OBJ}
+
+.PHONY: clean
+clean:
+	${RM} ${OBJ} ${DEP} main
+
+-include ${DEP}
+
+# END
---
+++ main.c
@@ -0,0 +1,41 @@
+#include <gtk/gtk.h>
+
+// Signal handler for the "clicked" signal of the start button.
+void hello(GtkMenuItem *item, gpointer user_data)
+{
+    g_print("Hello World\n");
+}
+
+// Main function.
+int main (int argc, char *argv[])
+{
+    gtk_init(NULL, NULL);
+
+  
+    GtkBuilder* builder = gtk_builder_new();
+    GError* error = NULL;
+    if (gtk_builder_add_from_file(builder, "./glade/app.glade", &error) == 0)
+    {
+        g_printerr("Error loading file: %s\n", error->message);
+        g_clear_error(&error);
+        return 1;
+    }
+
+    GtkGLArea* area = GTK_GL_AREA(gtk_builder_get_object(builder, "area"));
+    GtkWindow* window = GTK_WINDOW(gtk_builder_get_object(builder, "org.gtk.mandelbrot"));
+
+    GtkMenuBar *menubar = GTK_MENU_BAR(gtk_builder_get_object(builder, "menubar"));
+    GtkMenuItem *fileMenu = GTK_MENU_ITEM(gtk_builder_get_object(builder, "item_file"));
+    GtkMenuItem *btn_new = GTK_MENU_ITEM(gtk_builder_get_object(builder, "btn_new"));
+    GtkMenu *submenu_file = GTK_MENU(gtk_builder_get_object(builder, "submenu_file"));
+
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    g_signal_connect(G_OBJECT(btn_new), "activate", G_CALLBACK(hello), NULL);
+
+    gtk_widget_show_all(GTK_WIDGET(window));
+
+    gtk_main();
+
+    return 0;
+}
